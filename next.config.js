6cd63b99cd3869d17631a97f6d8b7462d7bc236b
memosys <82053242+memosys@users.noreply.github.com>
2023-07-29 17:01:36 +0530

    wss error attempt fix 1

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    APP_PROJECT_ID: process.env.APP_PROJECT_ID
-  },
-  webpack: config => {
+  webpack: (config) => {
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
