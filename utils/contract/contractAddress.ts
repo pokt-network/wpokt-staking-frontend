1e675637d56b33dfd4677cd0a9d5b17d2b49dce6
memosys <82053242+memosys@users.noreply.github.com>
2023-08-25 17:42:03 +0530

    update address

--- utils/contract/contractAddress.ts
+++ utils/contract/contractAddress.ts
@@ -1,4 +1,4 @@
 export const StakeContract = "0x61ae7a508Afeeaf4135f8BE4AEd4BB3cb8Ac9102";
 export const RewardContract = "0xE29C410564d31179130F051E29452857EC7ba667";
 export const StakingRewardContract =
-  "0x4620f9d278C521b09651fBa455d81F8053012182";
+  "0x55c2462e2c65fbdf74e3e81f8485d4e5afa925b9";
