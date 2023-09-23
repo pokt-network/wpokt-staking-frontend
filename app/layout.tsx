1342a1f51b6996573fc9ea981c6d0de3ee0fa697
memosys <82053242+memosys@users.noreply.github.com>
2023-09-23 15:50:39 +0530

    favicon update

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,10 @@ export default function RootLayout({
         <title>wPOKT Staking</title>
         <meta name="description" content="wPokt Staking Rewards App" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Manrope&display=swap" rel="stylesheet"></link>
       </Head>
       <body>
         <Provider>{children}</Provider>
